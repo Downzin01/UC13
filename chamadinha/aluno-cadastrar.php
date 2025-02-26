ba0d598cb142b74eff83e5b734c5d9aa0fd76add
Downzin01 <downmestre@gmail.com>
2025-02-26 12:00:06 -0300

    Commit Initial

---
+++ chamadinha/aluno-cadastrar.php
@@ -0,0 +1,40 @@
+<?php
+echo '<h1>Cadastro de Alunos</h1>';
+
+echo '<pre>';
+
+var_dump($_POST);
+
+$nomeFormulario = $_POST["nome"];
+$telefoneFormulario = $_POST["tel"];
+$emailFormulario = $_POST["email"];
+$nascFormulario = $_POST["nasc"];
+$frequenteFormulario = $_POST["frequente"];
+$imagemFormulario = $_POST["img"];
+
+$dsn = 'mysql:dbname=db_ti24;host=127.0.0.1';
+$user = 'root';
+$password = '';
+
+$banco = new PDO($dsn, $user, $password);
+
+$insert = "INSERT INTO tb_alunos(nome) VALUES(:nome);";
+$insertInfo = "INSERT INTO tb_info_alunos(telefone, email, nascimento, frequente, id_alunos, img) VALUES (:telefone, :email, :nascimento, :frequente, :id_alunos, :img);";
+
+$insertPreparado = $banco->prepare($insert);
+$insertInfoPreparado = $banco->prepare($insertInfo);
+
+$insertPreparado->execute([
+    ':nome' => $nomeFormulario
+]);
+
+$id_aluno = $banco->lastInsertId();
+$insertInfoPreparado->execute([
+    ':telefone' => $telefoneFormulario,
+    ':email' => $emailFormulario,
+    ':nascimento' => $nascFormulario,
+    ':frequente' => $frequenteFormulario,
+    ':id_alunos' => $id_aluno,
+    ':img' => $imagemFormulario,
+]);
+
---
+++ chamadinha/formulario.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Formulário</title>
+</head>
+<style>
+    body {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 10px;
+    }
+
+    .container {
+        padding: 20%;
+    }
+
+</style>
+<body>
+    <main class="container">
+        <h1>Formulário Pamonha</h1>
+        <form action="./aluno-cadastrar.php" method="POST">
+            <input type="text" name="nome" placeholder="nome">
+            <input type="number" name="tel" placeholder="telefone">
+            <input type="email" name="email" placeholder="email">
+            <input type="date" name="nasc" placeholder="nascimento">
+            <div>
+                <input type="checkbox" name="frequente">
+                <label for="">Frequente?</label>
+            </div>
+            <input type="file" name="img" accept="image/*">
+            <input type="submit">
+        </form>
+    </main>
+</body>
+</html>
